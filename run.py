3fad454a48faef89a25dceee421f630163cc2863
CrownedSilverFox <byskillcfpro@gmail.com>
2015-10-24 01:23:31 +0400

    To menu_1_2_2(Uncomplete).

--- run.py
+++ run.py
@@ -69,99 +69,57 @@ print("\033[1;34m* Welcome to %s %s *\033[1;m" % (school_data['number'], school_
 print("\033[1;34m*\033[1;m" * 24)
 menu_1()
 choice = input(':')
-
 if choice == '1':  # INFO
+# Menu_1_1
     clear()
     welcome_1()
     menu_2()
-# -----------------------------------------------
     choice = input(':')
     if choice == '1':
+    # Menu_1_1_1
         clear()
         welcome_1()
         menu_3()
 
 
     elif choice == '2':
+    #Menu_1_1_2
         clear()
-        print("*" * 24)
-        print("* Welcome to %s %s *" % (school_data['number'], school_data['type']))
-        print("*" * 24)
-        print("     MENU > Информация > Об учениках")
-        for class_room in school_data["classes"]:
-            print("Ученики '%s' класса: " % class_room)
-            for student in search(students_data, class_room=class_room):
-                # FIXME: учесть(во всей программе), в файле могут быть ученики из других школ
-                print("     ", get_full_name(student))  # TODO: Добавить нумерацию учеников для каждого класса
-            print("-" * 24)
-        input("Нажмите Enter для возврата в предыдущее меню")
+        welcome_1()
+
     elif choice == '3':
+    #Menu_1_1_3
         clear()
-        print("*" * 24)
-        print("* Welcome to %s %s *" % (school_data['number'], school_data['type']))
-        print("*" * 24)
-        print("     MENU > Информация > Об учителях")
-        print(' ... ')
-        # TODO: дописать вывод по аналогии с учениками
-        input("Нажмите Enter для возврата в предыдущее меню")
+        welcome_1()
+
 
 
 elif choice == '2':
+#Menu_1_2
     clear()
-    print("*" * 24)
-    print("* Welcome to %s %s *" % (school_data['number'], school_data['type']))
-    print("*" * 24)
-    print("     MENU > Редактировать")
-    print("1. Класс")
-    print("2. Ученика")
-    print("3. Учителя")
-    choice = input(": ")
+    welcome_1()
+
     if choice == '1':
+    # Menu_1_2_1
         clear()
-        print("*" * 24)
-        print("* Welcome to %s %s *" % (school_data['number'], school_data['type']))
-        print("*" * 24)
-        print("     MENU > Редактировать > Класс")
-        print("     Классы: ")
-        print("     ||", " || ".join(school_data['classes']), "||")
-        print()
-        print("1. Удалить существующий")
-        print("2. Создать новый")
-        print("3. Назад")  # TODO: Реализовать ВСЕ(во всей программе) пункты 'назад'
+        welcome_1()
+
 
         choice = input(": ")
         if choice == '1':
-            class_room = input("Введите класс: ")
-            if class_room in school_data["classes"]:
-                pass
-                # TODO: 1. Удалить класс из school.json
-                # TODO: 2. Удалить класс у всех учителей
-                # TODO: 3. Заменить класс у всех учеников на '' (считается что ученик ожидает перевод в новый класс)
-                # TODO: 4. Не забыть обновить информацию в файлах
-                # TODO: 5. Сделать изменения в меню 'MENU > Информация > Об учениках' (вывести учеников без классов)
+        # Menu_1_2_1_1
 
-        else:
-            print('Вы ввели несуществующий класс')
-            # TODO: и предложить ввести класс повторно
+
+
     elif choice == '2':
+    # Menu_1_2_2
         clear()
         print("*" * 24)
         print("* Welcome to %s %s *" % (school_data['number'], school_data['type']))
         print("*" * 24)
-        print("     MENU > Редактировать > Ученика")
-        for num, student in enumerate(students_data):
-            print("%s) %s || %s" % (num, get_full_name(student), student['class']))
-        student_num = input('Укажите номер ученика(или НОЛЬ, для создания нового): ')
-        print()
-        print("Вы выбрали %s " % 'unknown')  # TODO: Указать выбранного ученика и отобразить по нему полную информацию
-        # FIXME: не забыть обработать ввод номера несуществующего ученика
-        print('1. Удалить ученика')
-        print('2. Перевести в другой класс')
-        print('3. Назад')
-        # TODO: реализовать удаление и перевод ученика
-        # TODO*: реализовать создание нового ученика с вводом всех необходимых параметров
-        # TODO: не забыть, нельзя задать ученику несуществующий класс
+
     elif choice == '3':
+    #Menu_1_2_3
         clear()
         print("*" * 24)
         print("* Welcome to %s %s *" % (school_data['number'], school_data['type']))
@@ -174,8 +132,10 @@ elif choice == '2':
 
 
 elif choice == '3':
+#Menu_1_3
     print("Goodbye")
 else:
+#Menu_Exeption
     print("Error: Not correct menu item")
 
 
